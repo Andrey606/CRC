35d74da437a5e9be8da356c6c6f696a5d30ea43b
Andrey606 <akuluev@ukr.net>
2019-05-10 16:28:25 +0300

    crc-32 is calculated correctly

--- USER/inc/main.h
+++ USER/inc/main.h
@@ -16,11 +16,11 @@
 *
 *
 * Example:  0x44414548 -> 0xD9E49D9A
-*           0x0074E401 -> 0xF55A75A0
-*           0x00000500 -> 0x973DE075
-*           0x34363832 -> 0x244E2C4C
+*           0x0074E401 -> 0x7309BAAE
+*           0x00000500 -> 0x61DC3F57
+*           0x34363832 -> 0xA62C7754
 *           ...
-*                      -> 0x98A92CAC
+*                      -> 0x1dd6a505
 */
 
 #include <stdio.h> 
@@ -36,23 +36,16 @@ typedef __uint32_t uint32_t;
 
 // variables ----------------------------------------------------------------------
 
-const uint32_t CRC_result = 0x1DD6A505;
-
 #define CRC32_POLY   0x04C11DB7
-#define CRC32_POLY_R 0xEDB88320
-
-static uint32_t crc32_table[256];
-static uint32_t crc32r_table[256];
+uint32_t custom_crc_table[256];
+int byte_array[4];
 
 // prototypes ---------------------------------------------------------------------
 
 void write_file(uint32_t value);
-uint32_t calculate_crc(uint32_t *buffer, unsigned int length);
 uint32_t *load_data_from_file(const char *path_file, unsigned int *buf_size);
 long int filesize_bytes( FILE *fp );
 
-static void crc32_init(void);
-uint32_t crc32_byte(uint32_t init_crc, uint8_t *buf, int len);
-uint32_t crc32_stm32(uint32_t init_crc, uint32_t *buf, int len);
-
-uint32_t crc32(uint32_t crc, const void *buf, size_t size);
+uint32_t custom_crc32(uint32_t *buffer, unsigned int length);
+void generate_crc32_table(uint32_t poly);
+void int_to_bytes(uint32_t i);
